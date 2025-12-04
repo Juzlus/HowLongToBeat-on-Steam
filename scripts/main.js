288cd33b28833f7db6181b5ed45c919616f2e4dd
Juzlus <juzlus.biznes@gmail.com>
2025-12-04 02:30:15 +0100

    v1.9

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
-async function getCustomReplaces()
-{
+async function getCustomReplaces() {
     return new Promise((resolve, reject) => {
         chrome.runtime.sendMessage({ action: 'getCustomReplaces' }, response => {
             resolve(response?.success ? response.data : null);
@@ -7,8 +6,7 @@ async function getCustomReplaces()
     });
 }
 
-async function fetchHTML(url)
-{
+async function fetchHTML(url) {
     return new Promise((resolve, reject) => {
         chrome.runtime.sendMessage({ action: 'fetchHTML', url: url }, response => {
             resolve(response?.success ? response.data : null);
@@ -16,8 +14,7 @@ async function fetchHTML(url)
     });
 }
 
-async function searchHLTB(name)
-{
+async function searchHLTB(name) {
     return new Promise((resolve, reject) => {
         chrome.runtime.sendMessage({ action: 'searchHLTB', gameName: name }, response => {
             resolve(response?.success ? response.data : null);
@@ -25,8 +22,7 @@ async function searchHLTB(name)
     });
 }
 
-async function getPage(url)
-{
+async function getPage(url) {
     const response = await fetchHTML(url);
     if (!response) return;
     const parser = new DOMParser();
@@ -34,21 +30,19 @@ async function getPage(url)
     return doc;
 }
 
-async function getHLTBData(gameId)
-{
+async function getHLTBData(gameId) {
     const doc = await getPage(`https://howlongtobeat.com/game/${gameId}`);
     if (!doc) return;
     let scores = [];
     doc?.querySelectorAll("li[class*=GameStats_short__], li[class*=GameStats_long__], li[class*=GameStats_full__]")?.forEach((el, i) => {
         if (el?.querySelector('h4')?.innerText == "All Styles")
             return;
-        scores[i] = {name: el?.querySelector('h4')?.innerText, value: el?.querySelector('h5')?.innerText, timeColor: el?.classList[1]};
+        scores[i] = { name: el?.querySelector('h4')?.innerText, value: el?.querySelector('h5')?.innerText, timeColor: el?.classList[1] };
     });
     return scores;
 }
 
-function hasYear(name)
-{
+function hasYear(name) {
     const splited = name?.split(/[(||)]/);
     if (splited?.length >= 1)
         if (!isNaN(parseInt(splited[1])))
@@ -56,8 +50,7 @@ function hasYear(name)
     return [name, null]
 }
 
-async function searchByName(name)
-{
+async function searchByName(name) {
     let [gameName, year] = hasYear(name);
     if (!gameName) return;
     gameName = gameName?.replace(/[^a-zA-Z0-9 -_']/g, '');
@@ -72,19 +65,36 @@ async function searchByName(name)
     if (!response) return;
     const json = JSON.parse(response);
     if (!json || !json?.data?.length) return;
-    return year ? json?.data?.filter(el => el?.release_world == year)[0] : json?.data[0];
+    let title = year ? json?.data?.filter(el => el?.release_world == year)[0] : json?.data[0];
+    return title;
 }
 
-async function getUntranslatedTitle(steamId)
-{
+async function getUntranslatedTitle(steamId) {
     const doc = await getPage(`https://steamdb.info/api/RenderAppHover/?appid=${steamId}`);
     if (!doc) return;
     const title = doc?.querySelector('a.hover_title')?.innerText;
     return title;
-} 
+}
 
-async function createDiv() 
-{
+const waitForElm = async (selector) => {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        };
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+};
+
+async function createDiv() {
     const name = document.querySelector('.apphub_AppName')?.innerText;
     const steamId = document.querySelector('[data-miniprofile-appid]')?.getAttribute('data-miniprofile-appid');
     if (!name || !steamId) return;
@@ -104,10 +114,10 @@ async function createDiv()
         div.className = `large`;
     div.style = `--cover-img: url('https://howlongtobeat.com/games/${gameData?.game_image}')`;
     scores?.forEach(el => {
-        innerDiv += `<div style="width: ${100/scores?.length}%" class="${el?.timeColor}"><h4>${el?.name}</h4><h5>${el?.value}</h5></div>`;
+        innerDiv += `<div style="width: ${100 / scores?.length}%" class="${el?.timeColor}"><h4>${el?.name}</h4><h5>${el?.value}</h5></div>`;
     });
     div.innerHTML = innerDiv;
-    
+
     const steamdb = document.querySelector(".page_content .rightcol.game_meta_data .steamdb_stats");
     if (steamdb)
         steamdb.after(div);
@@ -115,4 +125,4 @@ async function createDiv()
         document.querySelector(".page_content .rightcol.game_meta_data").prepend(div);
 }
 
-createDiv();
+waitForElm('.apphub_AppName').then(() => createDiv());
