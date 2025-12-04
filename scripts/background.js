2bddda178f7b2da8f43b6c46bebea2cc3a40607b
Juzlus <juzlus.biznes@gmail.com>
2025-12-04 02:30:52 +0100

    v1.9

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,8 @@
 let customReplaces = null;
-let subPage = "lookup"
-const version = "1.8";
+let subPage = "search"
+const version = "1.9";
 
+let token = null;
 let apiUserKey = null;
 let apiSearchKey = null;
 let fetchData = null;
@@ -11,13 +12,13 @@ fetch('https://raw.githubusercontent.com/Juzlus/HowLongToBeat-on-Steam/refs/head
   .then(data => {
     if (!data) return;
     if (data?.version_firefox != version)
-    chrome.notifications.create('updateNotification', {
-      title: 'HowLongToBeat on Steam',
-      message: 'New version available. Click to download!',
-      priority: 1,
-      iconUrl: 'https://raw.githubusercontent.com/Juzlus/HowLongToBeat-on-Steam/refs/heads/main/icons/2048.png',
-      type: 'basic'
-    });
+      chrome.notifications.create('updateNotification', {
+        title: 'HowLongToBeat on Steam',
+        message: 'New version available. Click to download!',
+        priority: 1,
+        iconUrl: 'https://raw.githubusercontent.com/Juzlus/HowLongToBeat-on-Steam/refs/heads/main/icons/2048.png',
+        type: 'basic'
+      });
     customReplaces = data?.custom_replaces;
 
     chrome.notifications.onClicked.addListener((notifId) => {
@@ -26,7 +27,7 @@ fetch('https://raw.githubusercontent.com/Juzlus/HowLongToBeat-on-Steam/refs/head
         url: `https://github.com/Juzlus/HowLongToBeat-on-Steam/releases/latest/download/HowLongToBeat_on_Steam_v${data?.version_firefox}_[FireFox].zip`
       });
     });
-});
+  });
 
 async function getFetchData() {
   await fetch('https://raw.githubusercontent.com/Juzlus/HowLongToBeat-on-Steam/refs/heads/main/fetchData.txt')
@@ -47,7 +48,7 @@ async function getKey() {
       if (!_appUrl) return;
       appUrl = _appUrl;
     });
-  
+
   if (appUrl)
     await fetch(`https://howlongtobeat.com${appUrl}`)
       .then(response2 => response2.text())
@@ -58,7 +59,7 @@ async function getKey() {
           apiUserKey = userKey;
 
         const index = script.indexOf('searchOptions:');
-        const frag = script.slice(index - 400, index);
+        const frag = script.slice(index - 2000, index).replace("init?t=", "");
         subPage = frag.slice(frag.indexOf(`fetch("/api/`) + 12, frag.indexOf('/".concat'));
 
         const matches = [...frag.matchAll(/\.concat\(["']([^"']+)["']\)/g)];
@@ -72,7 +73,7 @@ getKey();
 getFetchData();
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
-  function(details) {
+  function (details) {
     details.requestHeaders.push({
       name: "Referer",
       value: "https://howlongtobeat.com/"
@@ -87,34 +88,60 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === 'getCustomReplaces') {
-      return sendResponse({ success: customReplaces ? true : false, data: customReplaces });
+    return sendResponse({ success: customReplaces ? true : false, data: customReplaces });
   }
   else if (message.action === 'fetchHTML') {
-      fetch(message.url)
-        .then(response => response.text())
-        .then(data => sendResponse({ success: true, data: data }))
-        .catch(error => sendResponse({ success: false, error: error.toString() }));
-      return true;
-  }
-  else if (message.action === 'searchHLTB') {
-    if (!apiUserKey && !apiSearchKey) return;
-    if (!fetchData) return;
-    let fetchDataCopy = fetchData.replace("{SEARCH_TERMS}", JSON.stringify(message?.gameName?.split(' ')));
-
-    if (apiUserKey)
-      fetchDataCopy = fetchData.replace("{USER_ID}", apiUserKey);
-
-    fetch(`https://howlongtobeat.com/api/${subPage}/${apiSearchKey ? `${apiSearchKey}` : ""}`, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        origin: "https://howlongtobeat.com/",
-      },
-      body: fetchDataCopy
-    })
+    fetch(message.url)
       .then(response => response.text())
       .then(data => sendResponse({ success: true, data: data }))
       .catch(error => sendResponse({ success: false, error: error.toString() }));
+    return true;
+  }
+  else if (message.action === 'searchHLTB') {
+    (async () => {
+      if (!fetchData) return sendResponse({ success: false });
+
+      if (subPage)
+        try {
+          const initFetch = await fetch(`https://howlongtobeat.com/api/${subPage + `/init?t=${Date.now()}`}`);
+          const initData = await initFetch.text();
+
+          if (initData.startsWith("{")) {
+            let tokenData = JSON.parse(initData);
+            if (tokenData?.token) token = tokenData.token;
+          }
+        } catch (e) { }
+
+      if (!token && !apiSearchKey)
+        return sendResponse({ success: false });
+
+      let fetchDataCopy = fetchData.replace("{SEARCH_TERMS}", JSON.stringify(message?.gameName?.split(' ')));
+
+      if (apiUserKey)
+        fetchDataCopy = fetchData.replace("{USER_ID}", apiUserKey);
+
+      let headers = {
+        "Content-Type": "application/json",
+        origin: "https://howlongtobeat.com/",
+      };
+
+      if (token)
+        headers["x-auth-token"] = token;
+
+      try {
+        const response = await fetch(`https://howlongtobeat.com/api/${subPage}/${apiSearchKey ? `${apiSearchKey}` : ""}`, {
+          method: 'POST',
+          headers: headers,
+          body: fetchDataCopy
+        });
+
+        const text = await response.text();
+        sendResponse({ success: true, data: text });
+      } catch (error) {
+        sendResponse({ success: false, error: error.toString() });
+      }
+    })();
+
     return true;
   }
 });
